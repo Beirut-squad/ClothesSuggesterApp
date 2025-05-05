bd243731bfaf4be3a6f2a32736e5c1825b03db0d
yousef_osama11 <oyousef709@gmail.com>
2025-05-05 18:12:09 +0300

    refactor weather api url

--- src/main/kotlin/data/utils/WeatherApiUrl.kt
+++ src/main/kotlin/data/utils/WeatherApiUrl.kt
@@ -1,8 +1,11 @@
 package org.example.data.utils
 
-object WeatherApiUrl {
-    var city: String = ""
-    var language: String = "en"
-    var baseUrl: String = "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=748f0749293fa00b514a285450d6581f&units=metric&lang=$language"
+import org.intellij.lang.annotations.Language
 
+object WeatherApiUrl {
+    fun getUrl(city: String = "cairo", language: String = "en"): String {
+        var baseUrl: String =
+            "https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=748f0749293fa00b514a285450d6581f&units=metric&lang=${language}"
+        return baseUrl
+    }
 }
