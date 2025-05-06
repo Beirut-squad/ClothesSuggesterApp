376b7260724a515e8e71422a383f844ebf50261c
tasneem-hakeem <tasniim.hakiim@gmail.com>
2025-05-06 19:29:51 +0300

    implement weather repository

--- src/main/kotlin/data/repositories/WeatherRepositoryImpl.kt
+++ src/main/kotlin/data/repositories/WeatherRepositoryImpl.kt
@@ -1,11 +1,17 @@
 package org.example.data.repositories
 
+import org.example.data.datasource.DataSource
+import org.example.data.mappers.toWeatherData
 import org.example.logic.models.WeatherData
 import org.example.logic.repositories.WeatherRepository
 
-class WeatherRepositoryImpl : WeatherRepository {
+class WeatherRepositoryImpl(
+    val dataSource: DataSource
+) : WeatherRepository {
 
-    override suspend fun getWeatherData(): WeatherData {
-        TODO("Not yet implemented")
+    override suspend fun getWeatherData(city: String, language: String): WeatherData {
+        val weatherData = dataSource.getWeatherData(city = city, language = language)
+
+        return weatherData.toWeatherData()
     }
 }
