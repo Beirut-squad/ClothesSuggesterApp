5f0df40d21c1361f43dcf8ac16d227292ed40cd9
tasneem-hakeem <tasniim.hakiim@gmail.com>
2025-05-07 01:31:19 +0300

    add koin modules

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,22 @@
 package org.example
 
+import org.example.di.dataModule
+import org.example.di.logicModule
+import org.example.di.uiModule
+import org.example.ui.MainMenuUi
+import org.koin.core.context.startKoin
+import org.koin.mp.KoinPlatform.getKoin
+
 fun main() {
-    println("Hello World!")
+    startKoin {
+        modules(
+            uiModule,
+            logicModule,
+            dataModule
+        )
+    }
+
+    val mainMenuUi: MainMenuUi = getKoin().get()
+    mainMenuUi.show()
 
 }
