f5be7df6609432f5d32fac5e3df63b8dee246830
tasneem-hakeem <tasniim.hakiim@gmail.com>
2025-05-07 19:16:10 +0300

    add case when user enters invalid option from main menu with testing

--- src/test/kotlin/ui/MainMenuUiTest.kt
+++ src/test/kotlin/ui/MainMenuUiTest.kt
@@ -3,12 +3,15 @@ package ui
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import io.mockk.verifyOrder
 import org.example.ui.ClothesSuggesterUi
 import org.example.ui.MainMenuUi
 import org.example.ui.components.Reader
 import org.example.ui.components.Viewer
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class MainMenuUiTest {
 
@@ -63,6 +66,48 @@ class MainMenuUiTest {
 
         // Then
         verify(exactly = 1) { viewer.printGoodbyeMessage("Goodbye :)") }
+    }
+
+    @Test
+    fun `should print error message when entering non numeric values for option`() {
+        // Given
+        every { reader.readInt() } returnsMany listOf(null, 2)
+
+        // When
+        mainMenuUi.show()
+
+        // Then
+        verifyOrder {
+            viewer.printWelcomeMessage("Welcome")
+            viewer.printTitle("Clothes Suggester App is ready")
+            viewer.printOptions(
+                "Suggest outfit for a person",
+                "Exit"
+            )
+            viewer.printInfoLine("Enter your choice:")
+            viewer.printError("Invalid option")
+        }
+    }
+
+    @Test
+    fun `should print error message when entering invalid number for option`() {
+        // Given
+        every { reader.readInt() } returnsMany listOf(50, 2)
+
+        // When
+        mainMenuUi.show()
+
+        // Then
+        verifyOrder {
+            viewer.printWelcomeMessage("Welcome")
+            viewer.printTitle("Clothes Suggester App is ready")
+            viewer.printOptions(
+                "Suggest outfit for a person",
+                "Exit"
+            )
+            viewer.printInfoLine("Enter your choice:")
+            viewer.printError("Invalid option")
+        }
     }
 
 }
